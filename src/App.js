775a1c1abeb2bcd58519a9b697b9e50de3ac3e3e
eregbene <benedicteregbene@gmail.com>
2023-05-19 16:55:08 +0300

    folder structure updated

--- src/App.js
+++ src/App.js
@@ -8,9 +8,19 @@ import Map from './components/Map/Map';
 const App =() => {
 
     return (
-        <div>
-            <h1>Hello, World!</h1>
-        </div>
+        <>
+        <CssBaseline />
+        <Header />
+        <Grid container spacing={3} style={{ width: '100%'}}>
+            <Grid item xs={12} md={4}>
+                <List />
+            </Grid>
+            <Grid item xs={12} md={8}>
+                <Map />
+            </Grid>
+       </Grid>
+            
+        </>
     );
 }
 
