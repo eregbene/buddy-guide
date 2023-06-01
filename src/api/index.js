a878e3ddb7ede63b38b6981b5906135c6c02ecc2
eregbene <77000067+eregbene@users.noreply.github.com>
2023-06-02 02:34:14 +0300

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -17,7 +17,7 @@ export const getPlacesData = async ( sw, ne) => {
     lang: 'en_US'
   },
   headers: {
-    'X-RapidAPI-Key': 'aabee70af9msh06393f9a0907150p1691adjsne2ed5dde3482',
+    'X-RapidAPI-Key': '',
     'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
   }
 });
