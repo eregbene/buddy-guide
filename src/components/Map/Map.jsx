1904265a443bf27c1c7ba4b9f81ed349b76e399c
eregbene <benedicteregbene@gmail.com>
2023-06-02 01:27:09 +0300

    display places on map

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -7,8 +7,8 @@ import Rating from '@material-ui/lab/';
 import useStyles from './styles.js';
 import { getPlacesData } from '../../api/index.js';
 
-const Map = ({setcoordinates, setBounds,coordinates}) => {
-  const isMobile = useMediaQuery('(min-width:600px)');
+const Map = ({setcoordinates, setBounds,coordinates, places}) => {
+  const isDesktop = useMediaQuery('(min-width:600px)');
   const classes = useStyles();
 
   return (
@@ -26,7 +26,36 @@ const Map = ({setcoordinates, setBounds,coordinates}) => {
         }}
         onChildClick={""}
       >
+     {places?.map((place,i) => (
+        <div
+          className={classes.markerContainer}
+            lat={Number(place.latitude)}
+            lng={Number(place.longitude)}
+            key={i}
+            >
+              {
+              
+                  !isDesktop ?(<LocationOnOutlinedIcon color="primary" fontSize="large" />
+              ): (
+                <Paper elevation={3} className={classes.paper}>
+                  <Typography className={classes.typography} variant="subtitle2" gutterBottom> 
+                    {place.name}
+          
+                  </Typography>
+                  <img
+                    className={classes.pointer}
+                    src={place.photo ? place.photo.images.large.url : 'https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg'}
+                  />
+                  
+                </Paper>
+              )}
+
+
+        </div>
+
+     )
      
+     )}
       </GoogleMapReact>
     </div>
   );
