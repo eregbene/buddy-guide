6b7170782f5cf897a86e399ef9734855f282e18b
eregbene <benedicteregbene@gmail.com>
2023-06-03 22:25:17 +0300

    final commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,27 +5,27 @@ import SearchIcon from '@material-ui/icons/Search';
 
 import useStyles from './styles.js';
 
-const Header = () => {
-    const classes = useStyles();
+const Header = ({ onPlaceChanged, onLoad }) => {
+  const classes = useStyles();
 
   return (
     <AppBar position="static">
       <Toolbar className={classes.toolbar}>
         <Typography variant="h5" className={classes.title}>
-          <b>Buddy Guide</b>
+          BUDDY TRAVEL
         </Typography>
         <Box display="flex">
           <Typography variant="h6" className={classes.title}>
-            See new places
+            Explore new places
           </Typography>
-          {/*<Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}>*/}
+          <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}>
             <div className={classes.search}>
               <div className={classes.searchIcon}>
                 <SearchIcon />
               </div>
               <InputBase placeholder="Search…" classes={{ root: classes.inputRoot, input: classes.inputInput }} />
             </div>
-         {/* </Autocomplete>*/}
+          </Autocomplete>
         </Box>
       </Toolbar>
     </AppBar>
